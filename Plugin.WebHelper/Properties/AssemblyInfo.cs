8368919cc551c1a17e2e7677c16d319b275aadab
DKorablin2 <danila.korablin@unity3d.com>
2025-02-23 18:03:52 +0300

    Unify plugins build

--- Plugin.WebHelper/Properties/AssemblyInfo.cs
+++ Plugin.WebHelper/Properties/AssemblyInfo.cs
@@ -1,7 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: ComVisible(false)]
 [assembly: Guid("117c606e-789a-47fd-8d9e-eefe6dab21d0")]
 [assembly: System.CLSCompliant(false)]
 
@@ -9,14 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyMetadata("ProjectUrl", "https://github.com/DKorablin/Plugin.WebHelper")]
 #else
 
-[assembly: AssemblyTitle("Plugin.WebHelper")]
 [assembly: AssemblyDescription("Various tools for Web administrators and programmers. ViewState Decoder original author: Fritz Onion")]
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
-[assembly: AssemblyCompany("Danila Korablin")]
-[assembly: AssemblyProduct("Plugin.WebHelper")]
-[assembly: AssemblyCopyright("Copyright © Danila Korablin 2010-2024")]
+[assembly: AssemblyCopyright("Copyright © Danila Korablin 2010-2025")]
 #endif
